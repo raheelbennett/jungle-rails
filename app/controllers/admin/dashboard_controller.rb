60f3c8ca89ddd00c4976d30177ef778d4d020291
Raheel Bennett <raheel.bennett@telus.com>
2023-01-26 16:58:10 -0700

    Replace the text on the dashboard page with general stats about the store

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
 http_basic_authenticate_with name: ENV['ADMIN_UN'], password: ENV['ADMIN_PW']
 
   def show
+    @products = Product.count
+    @categories = Category.count
   end
+
 end
